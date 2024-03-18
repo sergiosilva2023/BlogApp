ddf13c3390b917b94e3a1c33ebd27788e73af9b4
Sérgio Silva <150628460+sergiosilva2023@users.noreply.github.com>
2024-03-18 17:25:56 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports ={mongoURI: "mongodb+srv://sergioweb100:Vk3Rt8S3xQO67Rk1@blogapp-prod.dxuczpq.mongodb.net/?retryWrites=true&w=majority&appName=blogapp-prod"}
+    module.exports ={mongoURI: "mongodb+srv://sergioweb100:<usernameandpassword>.dxuczpq.mongodb.net/?retryWrites=true&w=majority&appName=blogapp-prod"}
 } else {
     module.exports = {mongoURI:"mongodb://localhost/blogapp"}
 }
